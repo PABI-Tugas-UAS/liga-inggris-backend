aa44a761e486c51827345030ad5004be7f89ed4a
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2024-12-22 15:39:22 +0700

    add: players module

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ use modules::clubs::controller as clubs_controller;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().merge(clubs_controller::register());
+    let app = Router::new()
+        .merge(clubs_controller::register())
+        .merge(players_controller::register());
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
