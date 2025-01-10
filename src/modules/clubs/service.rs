38c05d617ec59ac4f60cfd980b33296313ad82ef
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2025-01-10 23:03:57 +0700

    add: clubs detail on prev matches

--- src/modules/clubs/service.rs
+++ src/modules/clubs/service.rs
@@ -1,4 +1,5 @@
 use super::{entity::ClubReq, repository};
+use crate::modules::clubs::repository as club_repository;
 use crate::modules::matches::entity::MatchReq;
 use crate::modules::matches::repository as match_repository;
 use crate::modules::players::{entity::PlayerReq, repository as player_repository};
@@ -30,7 +31,25 @@ pub fn get_club_by_id(id: u64) -> Option<Value> {
     let previous_matches = match_repository::get_matches(Some(MatchReq {
         club_id: Some(id),
         status: Some("finished".to_string()),
-    }));
+    }))
+    .into_iter()
+    .map(|_match| {
+        let home_club = club_repository::get_club_by_id(_match.home_club_id);
+        let away_club = club_repository::get_club_by_id(_match.away_club_id);
+
+        serde_json::json!({
+            "id": _match.id,
+            "home_club": home_club,
+            "away_club": away_club,
+            "date": _match.date,
+            "time": _match.time,
+            "location": _match.location,
+            "status": _match.status,
+            "home_score": _match.home_score,
+            "away_score": _match.away_score,
+        })
+    })
+    .collect::<Vec<Value>>();
 
     repository::get_club_by_id(id).map(|club| {
         serde_json::json!({
